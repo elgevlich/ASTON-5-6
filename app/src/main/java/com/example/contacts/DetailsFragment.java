87a41c5d7f5ac3ea181a4a5f746eb1bfa22f7184
elgevlich <elgevlich@mail.ru>
2023-04-05 21:46:56 +0300

    Second additional task - search and delete

--- app/src/main/java/com/example/contacts/DetailsFragment.java
+++ app/src/main/java/com/example/contacts/DetailsFragment.java
@@ -20,98 +20,98 @@ import java.util.ArrayList;
 
 public class DetailsFragment extends Fragment {
 
-	private static final String NAME_KEY = "nameKey";
-	private static final String LASTNAME_KEY = "lastnameKey";
-	private static final String NUMBER_KEY = "numberKey";
-	private static final String ID_KEY = "IDKey";
-	private static final String IMAGE_KEY = "ImageKey";
+    private static final String NAME_KEY = "nameKey";
+    private static final String LASTNAME_KEY = "lastnameKey";
+    private static final String NUMBER_KEY = "numberKey";
+    private static final String ID_KEY = "IDKey";
+    private static final String IMAGE_KEY = "ImageKey";
 
-	private String name;
-	private String lastname;
-	private String number;
-	private String id;
-	private String image;
+    private String name;
+    private String lastname;
+    private String number;
+    private String id;
+    private String image;
 
-	TextInputLayout tilName;
-	TextInputLayout tilLastname;
-	TextInputLayout tilNumber;
-	TextView id_number;
-	ImageView contactImage;
-	EditText etName;
-	EditText etLastname;
-	EditText etNumber;
-	Button saveButton;
+    TextInputLayout tilName;
+    TextInputLayout tilLastname;
+    TextInputLayout tilNumber;
+    TextView id_number;
+    ImageView contactImage;
+    EditText etName;
+    EditText etLastname;
+    EditText etNumber;
+    Button saveButton;
 
-	@Override
-	public void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			name = getArguments().getString(NAME_KEY);
-			lastname = getArguments().getString(LASTNAME_KEY);
-			number = getArguments().getString(NUMBER_KEY);
-			id = getArguments().getString(ID_KEY);
-			image = getArguments().getString(IMAGE_KEY);
-		}
-	}
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            name = getArguments().getString(NAME_KEY);
+            lastname = getArguments().getString(LASTNAME_KEY);
+            number = getArguments().getString(NUMBER_KEY);
+            id = getArguments().getString(ID_KEY);
+            image = getArguments().getString(IMAGE_KEY);
+        }
+    }
 
-	@Override
-	public View onCreateView(
-		LayoutInflater inflater, ViewGroup container,
-		Bundle savedInstanceState
-	) {
-		return inflater.inflate(R.layout.fragment_details, container, false);
-	}
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
 
-	@Override
-	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		initViews(view);
-	}
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
 
 
-	public void initViews(View view) {
+    public void initViews(View view) {
 
-		tilName = view.findViewById(R.id.til_name);
-		tilLastname = view.findViewById(R.id.til_lastname);
-		tilNumber = view.findViewById(R.id.til_number);
-		id_number = view.findViewById(R.id.id_number);
+        tilName = view.findViewById(R.id.til_name);
+        tilLastname = view.findViewById(R.id.til_lastname);
+        tilNumber = view.findViewById(R.id.til_number);
+        id_number = view.findViewById(R.id.id_number);
 
-		tilName.getEditText().setText(name);
-		tilLastname.getEditText().setText(lastname);
-		tilNumber.getEditText().setText(number);
-		id_number.setText(id);
+        tilName.getEditText().setText(name);
+        tilLastname.getEditText().setText(lastname);
+        tilNumber.getEditText().setText(number);
+        id_number.setText(id);
 
-		etName = view.findViewById(R.id.et_name);
-		etLastname = view.findViewById(R.id.et_lastname);
-		etNumber = view.findViewById(R.id.et_number);
+        etName = view.findViewById(R.id.et_name);
+        etLastname = view.findViewById(R.id.et_lastname);
+        etNumber = view.findViewById(R.id.et_number);
 
-		contactImage = view.findViewById(R.id.image_contact);
-		Glide.with(requireContext()).load(image).into(contactImage);
+        contactImage = view.findViewById(R.id.image_contact);
+        Glide.with(requireContext()).load(image).into(contactImage);
 
-		saveButton = view.findViewById(R.id.save_button);
-		saveButton.setOnClickListener(v -> {
-			ArrayList<String> contactInfo = new ArrayList<>();
-			contactInfo.add(String.valueOf(etName.getText()));
-			contactInfo.add(String.valueOf(etLastname.getText()));
-			contactInfo.add(String.valueOf(etNumber.getText()));
-			contactInfo.add(id);
-			Bundle result = new Bundle();
-			result.putStringArrayList("bundleKey", contactInfo);
-			getParentFragmentManager().setFragmentResult("requestKey", result);
-			getParentFragmentManager().popBackStack();
-		});
-	}
+        saveButton = view.findViewById(R.id.save_button);
+        saveButton.setOnClickListener(v -> {
+            ArrayList<String> contactInfo = new ArrayList<>();
+            contactInfo.add(String.valueOf(etName.getText()));
+            contactInfo.add(String.valueOf(etLastname.getText()));
+            contactInfo.add(String.valueOf(etNumber.getText()));
+            contactInfo.add(id);
+            Bundle result = new Bundle();
+            result.putStringArrayList("bundleKey", contactInfo);
+            getParentFragmentManager().setFragmentResult("requestKey", result);
+            getParentFragmentManager().popBackStack();
+        });
+    }
 
-	public static DetailsFragment newInstance(String name, String lastname, String number, String id, String image) {
-		DetailsFragment detailsFragment = new DetailsFragment();
-		Bundle bundle = new Bundle();
-		bundle.putString(NAME_KEY, name);
-		bundle.putString(LASTNAME_KEY, lastname);
-		bundle.putString(NUMBER_KEY, number);
-		bundle.putString(ID_KEY, id);
-		bundle.putString(IMAGE_KEY, image);
-		detailsFragment.setArguments(bundle);
-		return detailsFragment;
-	}
+    public static DetailsFragment newInstance(String name, String lastname, String number, String id, String image) {
+        DetailsFragment detailsFragment = new DetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(NAME_KEY, name);
+        bundle.putString(LASTNAME_KEY, lastname);
+        bundle.putString(NUMBER_KEY, number);
+        bundle.putString(ID_KEY, id);
+        bundle.putString(IMAGE_KEY, image);
+        detailsFragment.setArguments(bundle);
+        return detailsFragment;
+    }
 
 }
--- app/src/main/java/com/example/contacts/ListFragment.java
+++ app/src/main/java/com/example/contacts/ListFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.Serializable;
@@ -24,124 +25,136 @@ import java.util.Map;
 
 public class ListFragment extends Fragment {
 
-	Map<String, Contact> map = new HashMap<>();
-	SearchView searchView;
+    Map<String, Contact> map = new HashMap<>();
+    SearchView searchView;
+    ContactAdapter adapter;
+    ArrayList<Contact> contactsList;
 
 
-	@Override
-	public void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		for (int i = 1; i < 110; i++) {
-			String url = "https://picsum.photos/id/" + i + "/1200/900";
-			String name = "Name" + i;
-			String lastname = "Lastname" + i;
-			String number = "+7 914 206 73 0" + i;
-			map.put(
-				String.valueOf(i),
-				new Contact(name, lastname, number, String.valueOf(i), url)
-			);
-		}
-	}
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        for (int i = 1; i < 110; i++) {
+            String url = "https://picsum.photos/id/" + i + "/1200/900";
+            String name = "Name" + i;
+            String lastname = "Lastname" + i;
+            String number = "+7 914 206 73 0" + i;
+            map.put(
+                    String.valueOf(i),
+                    new Contact(name, lastname, number, String.valueOf(i), url)
+            );
+        }
+    }
 
-	@Override
-	public void onSaveInstanceState(@NonNull Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putSerializable("list", (Serializable)map);
-	}
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("list", (Serializable) map);
+    }
 
-	@Override public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-		super.onViewStateRestored(savedInstanceState);
-		if (savedInstanceState != null) {
-			map = (Map<String, Contact>)savedInstanceState.getSerializable("list");
-		}
-	}
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        if (savedInstanceState != null) {
+            map = (Map<String, Contact>) savedInstanceState.getSerializable("list");
+        }
+    }
 
-	@Override
-	public View onCreateView(
-		LayoutInflater inflater, ViewGroup container,
-		Bundle savedInstanceState
-	) {
-		return inflater.inflate(R.layout.fragment_list, container, false);
-	}
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
 
-	@Override
-	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		ArrayList<Contact> contactsList = new ArrayList<>(map.values());
-		Collections.sort(contactsList);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        contactsList = new ArrayList<>(map.values());
+        Collections.sort(contactsList);
 
-		getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
-			ArrayList<String> result = bundle.getStringArrayList("bundleKey");
-			String id = result.get(3);
-			map.get(id).setName(result.get(0));
-			map.get(id).setLastname(result.get(1));
-			map.get(id).setNumber(result.get(2));
-			recyclerViewInit(view, contactsList);
-		});
-		recyclerViewInit(view, contactsList);
-	}
+        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
+            ArrayList<String> result = bundle.getStringArrayList("bundleKey");
+            String id = result.get(3);
+            map.get(id).setName(result.get(0));
+            map.get(id).setLastname(result.get(1));
+            map.get(id).setNumber(result.get(2));
+            recyclerViewInit(view, contactsList);
+        });
+        recyclerViewInit(view, contactsList);
+    }
 
-	private void recyclerViewInit(View view, ArrayList<Contact> contactsList) {
-		RecyclerView recyclerView = view.findViewById(R.id.contacts_list);
-		recyclerView.setHasFixedSize(true);
-		searchView = view.findViewById(R.id.contact_search);
-		ContactAdapter.OnContactClickListener onContactClickListener = (contact, position) -> {
-			DetailsFragment detailFragment = DetailsFragment.newInstance(
-				contact.getName(),
-				contact.getLastname(),
-				contact.getNumber(),
-				contact.getId(),
-				contact.getPicture()
-			);
-			FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-			if (view.findViewById(R.id.fragmentTablet) != null) {
-				fragmentManager.beginTransaction()
-					.replace(R.id.framelayout_right, detailFragment)
-					.addToBackStack("contacts")
-					.commit();
-			} else {
-				fragmentManager.beginTransaction()
-					.replace(R.id.framelayout_left, detailFragment)
-					.addToBackStack("contacts")
-					.commit();
-			}
-		};
-		ContactAdapter adapter = new ContactAdapter(contactsList, requireActivity(), onContactClickListener);
-		recyclerView.setAdapter(adapter);
+    private void recyclerViewInit(View view, ArrayList<Contact> contactsList) {
+        RecyclerView recyclerView = view.findViewById(R.id.contacts_list);
+        searchView = view.findViewById(R.id.contact_search);
 
-//		searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-//			override fun onQueryTextSubmit(query: String?): Boolean {
-//				return false
-//			}
-//
-//			override fun onQueryTextChange(newText: String?): Boolean {
-//				filterList(newText)
-//				return true
-//			}
-//
-//		})
-//
-//	}
-//
-//	private void filterList(String query) {
-//
-//		if (query != null) {
-//			ArrayList filteredList = new ArrayList<Contact>();
-//			for (i in contactsList) {
-//				if (i.title.lowercase(Locale.ROOT).contains(query)) {
-//					filteredList.add(i);
-//				}
-//			}
-//
-//			if (filteredList.isEmpty()) {
-//				Toast.makeText(requireContext(), "No Data found", Toast.LENGTH_SHORT).show();
-//			} else {
-//				adapter.setFilteredList(filteredList);
-//			}
-//		}
-//	}
+//        RecyclerView.ItemDecoration itemDecoration = new
+//                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL);
+//        recyclerView.addItemDecoration(itemDecoration);
 
-	}
+        ContactAdapter.OnContactClickListener onContactClickListener = (contact, position) -> {
+            DetailsFragment detailFragment = DetailsFragment.newInstance(
+                    contact.getName(),
+                    contact.getLastname(),
+                    contact.getNumber(),
+                    contact.getId(),
+                    contact.getPicture()
+            );
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            if (view.findViewById(R.id.fragmentTablet) != null) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.framelayout_right, detailFragment)
+                        .addToBackStack("contacts")
+                        .commit();
+            } else {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.framelayout_left, detailFragment)
+                        .addToBackStack("contacts")
+                        .commit();
+            }
+        };
+
+        ContactAdapter.OnLongClickListener onLongClickListener = (contact, position) -> {
+            contactsList.remove(position);
+            adapter.notifyItemRemoved(position);
+            adapter.notifyItemRangeChanged(position, contactsList.size());
+        };
+
+        adapter = new ContactAdapter(contactsList, requireActivity(), onContactClickListener, onLongClickListener);
+        recyclerView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return false;
+            }
+        });
+    }
+
+    private void filterList(String query) {
+
+        if (query != null) {
+            ArrayList filteredList = new ArrayList<Contact>();
+            for (Contact i : contactsList) {
+                if (i.getName().toLowerCase(Locale.ROOT).trim().contains(query) || i.getLastname().toLowerCase(Locale.ROOT).trim().contains(query)) {
+                    filteredList.add(i);
+                }
+            }
+
+            if (filteredList.isEmpty()) {
+                Toast.makeText(requireContext(), "No contacts found", Toast.LENGTH_SHORT).show();
+            } else {
+                adapter.setFilteredList(filteredList);
+            }
+        }
+    }
 
 }
 
