9e3421dc0c9369dee28b1ebb6f9bd37c78dcc2d6
elgevlich <elgevlich@mail.ru>
2023-04-06 10:16:02 +0300

    First additional task (fixed delete fun) Third additional task

--- app/src/main/java/com/example/contacts/ContactAdapter.kt
+++ app/src/main/java/com/example/contacts/ContactAdapter.kt
@@ -11,97 +11,98 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 class ContactAdapter(
-    private var contactsList: List<Contact>,
-    private val context: Context,
-    onContactClickListener: OnContactClickListener,
-    onLongClickListener: OnLongClickListener
+	private var contactsList: List<Contact>,
+	private val context: Context,
+	onContactClickListener: OnContactClickListener,
+	onLongClickListener: OnLongClickListener
 ) :
-    RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+	RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
 
-    interface OnContactClickListener {
+	interface OnContactClickListener {
 
-        fun onContactClick(contact: Contact?, position: Int)
-    }
+		fun onContactClick(contact: Contact?, position: Int)
+	}
 
-    interface OnLongClickListener {
+	interface OnLongClickListener {
 
-        fun onLongClick(contact: Contact?, position: Int)
+		fun onLongClick(contact: Contact?, position: Int)
 
-    }
-
-
-    private val onContactClickListener: OnContactClickListener
-    private val onLongClickListener: OnLongClickListener
+	}
 
 
-    init {
-        this.onContactClickListener = onContactClickListener
-    }
-
-    init {
-        this.onLongClickListener = onLongClickListener
-    }
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.item_contact, viewGroup, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(
-        viewHolder: ViewHolder,
-        @SuppressLint("RecyclerView") position: Int
-    ) {
-        val contact = contactsList[position]
-        viewHolder.name.text = contact.name
-        viewHolder.lastname.text = contact.lastname
-        viewHolder.number.text = contact.number
-        viewHolder.id.text = contact.id
-        Glide.with(context).load(contact.picture).into(viewHolder.image)
-
-        viewHolder.itemView.rootView
-            .setOnClickListener {
-                onContactClickListener.onContactClick(
-                    contact,
-                    position
-                )
-            }
-
-        viewHolder.itemView.rootView
-            .setOnLongClickListener {
-                onLongClickListener.onLongClick(
-                    contact, position
-                )
-                true
-            }
-    }
-
-    override fun getItemCount(): Int {
-        return contactsList.size
-    }
-
-    fun setFilteredList(contactsList: List<Contact>) {
-        this.contactsList = contactsList
-        notifyDataSetChanged()
-    }
+	private val onContactClickListener: OnContactClickListener
+	private val onLongClickListener: OnLongClickListener
 
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+	init {
+		this.onContactClickListener = onContactClickListener
+	}
 
-        val name: TextView
-        val lastname: TextView
-        val number: TextView
-        val id: TextView
-        val image: ImageView
+	init {
+		this.onLongClickListener = onLongClickListener
+	}
 
-        init {
-            name = view.findViewById(R.id.name)
-            lastname = view.findViewById(R.id.lastname)
-            number = view.findViewById(R.id.number)
-            image = view.findViewById(R.id.image_contact)
-            id = view.findViewById(R.id.id_number)
-        }
-    }
+	override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+		val view = LayoutInflater.from(viewGroup.context)
+			.inflate(R.layout.item_contact, viewGroup, false)
+		return ViewHolder(view)
+	}
+
+	override fun onBindViewHolder(
+		viewHolder: ViewHolder,
+		@SuppressLint("RecyclerView") position: Int
+	) {
+		val contact = contactsList[position]
+		viewHolder.name.text = contact.name
+		viewHolder.lastname.text = contact.lastname
+		viewHolder.number.text = contact.number
+		viewHolder.id.text = contact.id
+		Glide.with(context).load(contact.picture).into(viewHolder.image)
+
+		viewHolder.itemView.rootView
+			.setOnClickListener {
+				onContactClickListener.onContactClick(
+					contact,
+					position
+				)
+			}
+
+		viewHolder.itemView.rootView
+			.setOnLongClickListener {
+				onLongClickListener.onLongClick(
+					contact, position
+				)
+				true
+			}
+	}
+
+	override fun getItemCount(): Int {
+		return contactsList.size
+	}
+
+	@SuppressLint("NotifyDataSetChanged")
+	fun setFilteredList(contactsList: List<Contact>) {
+		this.contactsList = contactsList
+		notifyDataSetChanged()
+	}
+
+
+	class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+		val name: TextView
+		val lastname: TextView
+		val number: TextView
+		val id: TextView
+		val image: ImageView
+
+		init {
+			name = view.findViewById(R.id.name)
+			lastname = view.findViewById(R.id.lastname)
+			number = view.findViewById(R.id.number)
+			image = view.findViewById(R.id.image_contact)
+			id = view.findViewById(R.id.id_number)
+		}
+	}
 
 
 }
--- app/src/main/java/com/example/contacts/ListFragment.java
+++ app/src/main/java/com/example/contacts/ListFragment.java
@@ -25,136 +25,143 @@ import java.util.Map;
 
 public class ListFragment extends Fragment {
 
-    Map<String, Contact> map = new HashMap<>();
-    SearchView searchView;
-    ContactAdapter adapter;
-    ArrayList<Contact> contactsList;
+	Map<String, Contact> map = new HashMap<>();
+	SearchView searchView;
+	ContactAdapter adapter;
+	ArrayList<Contact> contactsList;
 
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        for (int i = 1; i < 110; i++) {
-            String url = "https://picsum.photos/id/" + i + "/1200/900";
-            String name = "Name" + i;
-            String lastname = "Lastname" + i;
-            String number = "+7 914 206 73 0" + i;
-            map.put(
-                    String.valueOf(i),
-                    new Contact(name, lastname, number, String.valueOf(i), url)
-            );
-        }
-    }
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContactList(map);
+	}
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putSerializable("list", (Serializable) map);
-    }
+	@Override
+	public void onSaveInstanceState(@NonNull Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putSerializable("list", (Serializable)map);
+	}
 
-    @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
-        if (savedInstanceState != null) {
-            map = (Map<String, Contact>) savedInstanceState.getSerializable("list");
-        }
-    }
+	@Override
+	public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+		super.onViewStateRestored(savedInstanceState);
+		if (savedInstanceState != null) {
+			map = (Map<String, Contact>)savedInstanceState.getSerializable("list");
+		}
+	}
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
-    }
+	@Override
+	public View onCreateView(
+		LayoutInflater inflater, ViewGroup container,
+		Bundle savedInstanceState
+	) {
+		return inflater.inflate(R.layout.fragment_list, container, false);
+	}
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        contactsList = new ArrayList<>(map.values());
-        Collections.sort(contactsList);
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
 
-        getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
-            ArrayList<String> result = bundle.getStringArrayList("bundleKey");
-            String id = result.get(3);
-            map.get(id).setName(result.get(0));
-            map.get(id).setLastname(result.get(1));
-            map.get(id).setNumber(result.get(2));
-            recyclerViewInit(view, contactsList);
-        });
-        recyclerViewInit(view, contactsList);
-    }
 
-    private void recyclerViewInit(View view, ArrayList<Contact> contactsList) {
-        RecyclerView recyclerView = view.findViewById(R.id.contacts_list);
-        searchView = view.findViewById(R.id.contact_search);
+		getParentFragmentManager().setFragmentResultListener("requestKey", this, (requestKey, bundle) -> {
+			ArrayList<String> result = bundle.getStringArrayList("bundleKey");
+			String id = result.get(3);
+			map.get(id).setName(result.get(0));
+			map.get(id).setLastname(result.get(1));
+			map.get(id).setNumber(result.get(2));
+			recyclerViewInit(view, contactsList);
+		});
+		recyclerViewInit(view, contactsList);
+	}
 
-//        RecyclerView.ItemDecoration itemDecoration = new
-//                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL);
-//        recyclerView.addItemDecoration(itemDecoration);
+	private void recyclerViewInit(View view, ArrayList<Contact> contactsList) {
+		RecyclerView recyclerView = view.findViewById(R.id.contacts_list);
+		searchView = view.findViewById(R.id.contact_search);
+		recyclerView.setHasFixedSize(true);
 
-        ContactAdapter.OnContactClickListener onContactClickListener = (contact, position) -> {
-            DetailsFragment detailFragment = DetailsFragment.newInstance(
-                    contact.getName(),
-                    contact.getLastname(),
-                    contact.getNumber(),
-                    contact.getId(),
-                    contact.getPicture()
-            );
-            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
-            if (view.findViewById(R.id.fragmentTablet) != null) {
-                fragmentManager.beginTransaction()
-                        .replace(R.id.framelayout_right, detailFragment)
-                        .addToBackStack("contacts")
-                        .commit();
-            } else {
-                fragmentManager.beginTransaction()
-                        .replace(R.id.framelayout_left, detailFragment)
-                        .addToBackStack("contacts")
-                        .commit();
-            }
-        };
+		RecyclerView.ItemDecoration itemDecoration = new
+			DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL);
+		recyclerView.addItemDecoration(itemDecoration);
 
-        ContactAdapter.OnLongClickListener onLongClickListener = (contact, position) -> {
-            contactsList.remove(position);
-            adapter.notifyItemRemoved(position);
-            adapter.notifyItemRangeChanged(position, contactsList.size());
-        };
+		ContactAdapter.OnContactClickListener onContactClickListener = (contact, position) -> {
+			DetailsFragment detailFragment = DetailsFragment.newInstance(
+				contact.getName(),
+				contact.getLastname(),
+				contact.getNumber(),
+				contact.getId(),
+				contact.getPicture()
+			);
+			FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+			if (view.findViewById(R.id.fragmentTablet) != null) {
+				fragmentManager.beginTransaction()
+					.replace(R.id.framelayout_right, detailFragment)
+					.addToBackStack("contacts")
+					.commit();
+			} else {
+				fragmentManager.beginTransaction()
+					.replace(R.id.framelayout_left, detailFragment)
+					.addToBackStack("contacts")
+					.commit();
+			}
+		};
 
-        adapter = new ContactAdapter(contactsList, requireActivity(), onContactClickListener, onLongClickListener);
-        recyclerView.setAdapter(adapter);
+		ContactAdapter.OnLongClickListener onLongClickListener = (contact, position) -> {
+			contactsList.remove(position);
+			adapter.notifyItemRemoved(position);
+			adapter.notifyItemRangeChanged(position, contactsList.size());
+		};
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
+		adapter = new ContactAdapter(contactsList, requireActivity(), onContactClickListener, onLongClickListener);
+		recyclerView.setAdapter(adapter);
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                filterList(newText);
-                return false;
-            }
-        });
-    }
+		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
-    private void filterList(String query) {
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				return false;
+			}
 
-        if (query != null) {
-            ArrayList filteredList = new ArrayList<Contact>();
-            for (Contact i : contactsList) {
-                if (i.getName().toLowerCase(Locale.ROOT).trim().contains(query) || i.getLastname().toLowerCase(Locale.ROOT).trim().contains(query)) {
-                    filteredList.add(i);
-                }
-            }
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				filterList(newText);
+				return false;
+			}
+		});
+	}
 
-            if (filteredList.isEmpty()) {
-                Toast.makeText(requireContext(), "No contacts found", Toast.LENGTH_SHORT).show();
-            } else {
-                adapter.setFilteredList(filteredList);
-            }
-        }
-    }
+	private void filterList(String query) {
+
+		if (query != null) {
+			ArrayList<Contact> filteredList = new ArrayList<>();
+			for (Contact i : contactsList) {
+				if (i.getName().toLowerCase(Locale.ROOT).trim().contains(query) || i.getLastname().toLowerCase(Locale.ROOT).trim().contains(query)) {
+					filteredList.add(i);
+				}
+			}
+
+			if (filteredList.isEmpty()) {
+				Toast.makeText(requireContext(), "No contacts found", Toast.LENGTH_SHORT).show();
+			} else {
+				adapter.setFilteredList(filteredList);
+			}
+		}
+	}
+
+	private void setContactList(Map<String, Contact> map) {
+		for (int i = 1; i < 110; i++) {
+			String url = "https://picsum.photos/id/" + i + "/1200/900";
+			String name = "Name" + i;
+			String lastname = "Lastname" + i;
+			String number = "+7 914 206 73 0" + i;
+			map.put(
+				String.valueOf(i),
+				new Contact(name, lastname, number, String.valueOf(i), url)
+			);
+		}
+		contactsList = new ArrayList<>(map.values());
+		Collections.sort(contactsList);
+	}
 
 }
 
